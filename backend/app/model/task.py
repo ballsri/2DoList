9e1d8b5d7279fc3522bdf0f28d85b29775d42f05
DESKTOP-FLD7G2E\ball0 <ball04407@hotmail.com>
2023-02-05 06:38:33 +0700

    Fix timestamp bug

--- backend/app/model/task.py
+++ backend/app/model/task.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from db_config import db, Base
 from sqlalchemy import Table, Column,Boolean,ForeignKey, Integer,Text ,  String,Enum, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 import enum
 from datetime import datetime
 
@@ -16,7 +17,7 @@ class Task(Base):
     title = Column(String(50), nullable=False)
     description = Column(Text, nullable=True)
     importantLevel = Column(Enum(ILevel), nullable=False)
-    createDate = Column(DateTime, default=datetime.utcnow())
+    createDate = Column(DateTime, server_default=func.current_timestamp())
     dueDate = Column(DateTime, nullable=False)
     projectId = Column(String(36), ForeignKey("projects.id"), nullable=False) 
     from_project = relationship('Project', back_populates="tasks")
